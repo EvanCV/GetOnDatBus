2fd746ef94d2e7ed80fb20c7812cc324c30a2ad3
EvanCV <evan.c.vandenberg@gmail.com>
2015-01-20 15:55:56 -0800

    first commit

---
+++ GetOnDatBus/BusStopDetails.h
@@ -0,0 +1,13 @@
+//
+//  BusStopDetails.h
+//  GetOnDatBus
+//
+//  Created by Evan Vandenberg on 1/20/15.
+//  Copyright (c) 2015 Evan Vandenberg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BusStopDetails : NSObject
+
+@end
---
+++ GetOnDatBus/BusStopDetails.m
@@ -0,0 +1,13 @@
+//
+//  BusStopDetails.m
+//  GetOnDatBus
+//
+//  Created by Evan Vandenberg on 1/20/15.
+//  Copyright (c) 2015 Evan Vandenberg. All rights reserved.
+//
+
+#import "BusStopDetails.h"
+
+@implementation BusStopDetails
+
+@end
