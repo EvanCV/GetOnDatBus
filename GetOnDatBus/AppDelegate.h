b2c9482731913173c7baa34aac809e2c1ca190c1
EvanCV <evan.c.vandenberg@gmail.com>
2015-01-20 14:01:01 -0800

    Initial Commit

---
+++ GetOnDatBus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetOnDatBus
+//
+//  Created by Evan Vandenberg on 1/20/15.
+//  Copyright (c) 2015 Evan Vandenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
